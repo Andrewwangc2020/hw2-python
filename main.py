b1b7dc645d9591af94f4cb5c53b9ebce38ef83ce
Andrewwangc2020 <Andrewwang.c2020@gmail.com>
2020-09-12 02:10:26 +0000

    getGradePoint takes argument

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #Author: Andrew Wang aqw5628@psu.edu
 
-def getGradePoint():
+def getGradePoint(lettergrade):
   if lettergrade == "A":
     return 4.0;
   elif lettergrade == "A-":
@@ -20,24 +20,24 @@ def getGradePoint():
   else:
     return 0.0;
 
-lettergrade = input("Enter your course 1 letter grade: ");
-gradepoint1 = getGradePoint();
+letter1 = input("Enter your course 1 letter grade: ");
+gradepoint1 = getGradePoint(letter1);
 c = input("Enter your course 1 credit: ");
 credit1 = int(c);
 print(f"Grade point for course 1 is: {gradepoint1}");
 
-lettergrade = input("Enter your course 2 letter grade: ");
-gradepoint2 = getGradePoint();
+letter2 = input("Enter your course 2 letter grade: ");
+gradepoint2 = getGradePoint(letter2);
 c = input("Enter your course 2 credit: ");
 credit2 = int(c);
 print(f"Grade point for course 2 is: {gradepoint2}");
 
-lettergrade = input("Enter your course 3 letter grade: ");
-gradepoint3 = getGradePoint();
+letter3 = input("Enter your course 3 letter grade: ");
+gradepoint3 = getGradePoint(letter3);
 c = input("Enter your course 3 credit: ");
 credit3 = int(c);
 print(f"Grade point for course 3 is: {gradepoint3}");
 
-gpa = (gradepoint1 * credit1 + gradepoint2 * credit2 + gradepoint3 * credit3) / (credit1 + credit2 + credit3) 
+gpa = (gradepoint1 * credit1 + gradepoint2 * credit2 + gradepoint3 * credit3) / (credit1 + credit2 + credit3); 
 print(f"Your GPA is: {gpa}");
 
